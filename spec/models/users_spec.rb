5f1685920a21b8c405cbdf73794973df624b1871
Kara Kelley & Nicole Cody <pair+klkelley.ncody@devbootcamp.com>
2017-10-21 15:48:19 -0500

    Add additional user tests for user location

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'User' do
-  let(:user) {User.new(name: "Nicole", interest: "nothing", email: "nicole@nicole.com", password: "password")}
+  let(:user) {User.new(name: "Nicole", interest: "nothing", email: "nicole@nicole.com", password: "password", longitude: 87.45345, latitude: -77.234234)}
 
   it 'has a name' do
     expect(user.name).to eq "Nicole"
@@ -17,6 +17,14 @@ describe 'User' do
 
   it 'has an interest' do
     expect(user.interest).to eq "nothing"
+  end
+
+  it 'has a longitude' do
+    expect(user.longitude).to eq 87.45345
+  end
+
+  it 'has a latitude' do
+    expect(user.latitude).to eq -77.234234
   end
 
 end
