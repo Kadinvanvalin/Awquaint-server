191c5a881f0c10dbc687e49b46211e3c1537919c
Kara Kelley & Nicole Cody <pair+klkelley.ncody@devbootcamp.com>
2017-10-21 15:44:49 -0500

    Add longitude and latitude to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
 
   has_secure_password
+
+  def self.nearby(latitude, longitude)
+    max_latitude = latitude + 0.0009
+    max_longitude = longitude + 0.0009
+    min_latitude = latitude - 0.0009
+    min_longitude = longitude - 0.0009
+
+    nearby_users = []
+    User.all.each do |user|
+      if user.longitude > min_longitude && user.longitude < max_longitude
+        if user.latitude > min_latitude && user.latitude < max_latitude
+          nearby_users << user
+        end
+      end
+    end
+    nearby_users
+  end
+
+  def current_user
+
+  end
 end
