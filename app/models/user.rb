77f1142f5043414d444b51b571ce089dc26e4f33
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-10-25 09:44:48 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :name, :email, presence: true
   validates :email, uniqueness: true
 
-  has_attached_file :image, styles: { thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "300x300>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   has_secure_password
